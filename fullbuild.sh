f66aab39fafe79e81e26d3fede0953436eb7fe4e
Ronny Trommer <ronny@opennms.org>
2017-02-20 15:22:10 +0100

    Use opennms.home instead hard coded /opt/opennms

--- fullbuild.sh
+++ fullbuild.sh
@@ -20,4 +20,4 @@ cd ${OPENNMS_SRC}/opennms-full-assembly
 mvn -Dbuild.profile=fulldir -Droot.dir=${OPENNMS_SRC} -Dopennms.home=${OPENNMS_HOME} -Dmaven.metadata.legacy=true -Djava.awt.headless=true ${@} install
 
 cd ${OPENNMS_SRC}/target
-ln -s opennms-**/ /opt/opennms
+ln -s opennms-**/ ${OPENNMS_HOME}
